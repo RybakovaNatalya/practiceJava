aaaa84b6bad266b008d705c2ccd45608a91e8d73
RybakovaNatalya <58661891+RybakovaNatalya@users.noreply.github.com>
2022-02-21 00:14:40 +0300

    add task 13

---
+++ YourString.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.util.Scanner;
+
+public class YourString {
+    private static String str;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите строку из слов, разделенных пробелами");
+        String str = in.nextLine();
+
+
+        String new_str = str.replaceAll("[^a-zA-Z\\s]", "");
+
+        // удаляем лишние пробелы
+        while (new_str.contains("  ")) {
+            String res = new_str.replace("  ", " ");
+            new_str = res;
+        }
+
+
+        String[] count = new_str.split(" ");
+
+
+        System.out.println(new_str);
+        System.out.print("Количество слов: " + count.length);
+    }
+}
